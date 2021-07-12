c6cd776030ec48f035d5fe070700dd8e4d8b7b10
nikhil <vekatnikhil1234@msitprogram.net>
2021-07-12 16:46:04 +0530

    distance

--- 03-distance-Python/distance.py
+++ 03-distance-Python/distance.py
@@ -6,4 +6,9 @@
 
 def fun_distance(x1, y1, x2, y2):
 	# your code goes here
-	return 0
+	m=(x2-x1)**2
+	n=(y2-y1)**2
+	a=(m+n)**(1/2)
+	# print(a)
+	return (int(a))
+# print(fun_distance(int(input()),int(input()),int(input()),int(input())))
