ad56878af32c599d32b25ef2ab83087f0387392f
nikhil <vekatnikhil1234@msitprogram.net>
2021-07-15 11:26:06 +0530

    mostfrequent

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -4,4 +4,27 @@
 
 def mostfrequentdigit(n):
 	# your code goes here
-	pass
+	if n==0:
+		return 0
+	n=abs(n)
+	count=n
+	m=0
+	k=0
+	while n>0:
+		temp=count
+		a=n%10
+		verify=0
+		while temp>0:
+			b=temp%10
+			if b==a:
+				verify=verify+1
+			temp=temp//10
+		if m<verify:
+			m=verify
+			k=a
+		if m==verify:
+			if a<k:
+				k=a
+		n=n//10
+	return k
+	
