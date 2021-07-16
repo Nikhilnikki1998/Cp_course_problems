d97245e2e3b80f1685a1630e2c5e69699c4bd6a3
nikhil <vekatnikhil1234@msitprogram.net>
2021-07-16 09:45:33 +0530

    circle

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -6,4 +6,13 @@
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	# your code goes here
-	return False 
+	a=(((x2-x1)**2)+((y2-y1)**2))
+	a=((a)**(1/2))
+	b=r1+r2
+	c=r1-r2
+	if(int(a)==b or int(a)==c):
+		return True
+	elif(int(a)<b or int(a)<c):
+		return True
+	else:
+		return False 
