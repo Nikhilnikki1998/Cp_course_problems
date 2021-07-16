372a81e46bae84b5e4d3f393b57ae182cecdd307
nikhil <vekatnikhil1234@msitprogram.net>
2021-07-16 11:01:12 +0530

    apply

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -10,8 +10,39 @@
 # assert(applyCaesarCipher("zodiac", -2) == "xmbgya")
 
 
+
 def fun_applycaesarcipher(msg, shift):
-	return ""
+    st=""
+    for i in msg:
+        # print(i)
+        if(i==" "):
+            # print(i)
+            st+=i
+
+            continue
+        ascii=ord(i)
+        if(97<=ascii and ascii<=122):
+            
+            b=ascii+shift
+            if(b>122):
+                b=b-122
+                b=96+b
+            elif(b<97):
+                b=97-b
+                b=123-b
+        elif(65<=ascii and ascii<=90):
+            b=ascii+shift
+            if(b>90):
+                b=b-90
+                b=64+b
+            elif(b<65):
+                b=65-b
+                b=91-b
+        a=chr(b)
+        st+=a
+        # print(st)
+    return st
+ 
 
 
 
