96ab224281ba3d32367bbeba9071d381747b8ae5
nikhil <vekatnikhil1234@msitprogram.net>
2021-07-22 09:05:27 +0530

    matrix

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -2,10 +2,22 @@
 # (that we will consider to be matrices) and returns a new 2d list that 
 # is the result of multiplying the two matrices. Return None if the 
 # two matrices cannot be multiplied for any reason.
-
+def listing(r,c):
+    return [([0]*c) for x in range(r)]
 
 def fun_matrixmultiply(m1, m2):
-    return None
+    a=len(m1)
+    b=len(m2)
+    for i in range(a):
+        if len(m1[i])!=b:
+            return None
+    c=listing(a,len(m2[0]))
+    for i in range(a):
+        for j in range(len(m2[0])):
+            c[i][j]=0
+            for v in range(b):
+                c[i][j]=c[i][j]+m1[i][v]*m2[v][j]
+    return c
 
 
 
