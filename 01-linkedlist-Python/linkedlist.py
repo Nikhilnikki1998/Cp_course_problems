f7fc086f886f210e72d560b9f67e3c257cb4ae40
nikhil <vekatnikhil1234@msitprogram.net>
2021-07-26 10:05:44 +0530

    linked

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -19,22 +19,76 @@ class LinkedList(object):
         
     def append(self, new_element):
         # Your code goes here
-        pass
+        new_node = Element(new_element)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while (last.next):
+            last = last.next
+        last.next =  new_node
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
-    
+        current = self.head
+        count = 0
+        while (current):
+            if (count == position):
+                return current.data
+            count += 1
+            current = current.next
+
+        return None
+    def getNode(self,data):
+ 
+    # allocating space
+        newNode = Element(data)
+        return newNode
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
+        
+        head = self.head
+        
+        # This condition to check whether the
+        # position given is valid or not.
+        if (position < 1):       
+            print("Invalid position!")
+        
+        if position == 1:
+            newNode = Element(new_element)
+            newNode.next = self.head
+            head = newNode
+                
+        else:
+        
+            # Keep looping until the position is zero
+            while (position != 0):          
+                position -= 1
+    
+                if (position == 1):
+                    newNode = getNode(new_element)
+
+                    newNode.next = self.head.next
+    
+                # Replacing headNode with new Node
+                # to the old Node to point to the new Node
+                    self.head.next = newNode
+                    break
+                
+                self.head = self.head.next
+                if self.head == None:
+                    break           
+            if position != 1:
+                print("position out of range")       
+        return head
+            
     
     
     def delete(self, value):
