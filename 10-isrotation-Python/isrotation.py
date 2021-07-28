9328acaa622e0cdde0a30c18a6ef56a262b4ad6f
nikhil <vekatnikhil1234@msitprogram.net>
2021-07-28 20:47:41 +0530

    rott

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -5,6 +5,25 @@
 # 3412 is a rotation of 1234. Any number 
 # is a rotation of itself.
 
+def rightrotation(r):
+    m=len(r)
+    z=r[m-1:]+r[:m-1]
+    return z
 def isrotation(x, y):
+    a=[]
+    b=[]
+    while(x>0):
+        a.append(x%10)
+        x=x//10
+    while(y>0):
+        b.append(y%10)
+        y=y//10
+    # your code goes here
+    n=len(a)
+    for i in range(n):
+        a=rightrotation(a)
+        if(a==b):
+            return True
+    return False
 	
 	
