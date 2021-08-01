f1aee734e2fa71f1e5759c65f56f95003ca5d44d
nikhil <vekatnikhil1234@msitprogram.net>
2021-08-01 21:21:50 +0530

    prime

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -2,6 +2,29 @@
 # and returns True if L does not contain any primes, and False otherwise.
 
 
+def prime(k):
+    if(k<2):
+        return False
+    elif(k==2):
+        return True
+    elif(k%2==0):
+        return False
+    else:
+        for i in range(3,int((k)),2):
+            if(k%i==0):
+                return False
+            
+        return True
+    
+
 def fun_hasnoprimes(l):
-	return True
+    for i in l:
+        # print(i)
+        for j in i :
+            # print(j)
+            if(prime(j)==True):
+                return False
+            
+    return True
+print(fun_hasnoprimes([[12,4,6],[8,12,14],[6,18]]))
 
