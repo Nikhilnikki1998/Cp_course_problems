8932c770bff88e361828eeb9b6a962173251efe5
nikhil <vekatnikhil1234@msitprogram.net>
2021-08-01 22:09:42 +0530

    tidy

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,5 +6,20 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 
+def tidy(n):
+    a=n%10
+    b=n//10
+    if(a>b):
+        return True
+    else:
+        return False
+
 def fun_nth_tidynumber(n):
-    return 0
+    count=-1
+    if(n==0):
+        return 1
+    if(tidy(n)==True):
+        count=count+1
+    if(count==n):
+        return count
+print(fun_nth_tidynumber(1))
